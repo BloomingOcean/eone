4594588f98fef0ed056b7afe81ffb1796036d5db
RobberClc <1519365158@qq.com>
2021-05-24 17:46:21 +0800

    数据统计功能(包含定时执行任务)

---
+++ service/service_statistics/src/main/java/com/liyang/staservice/mapper/StatisticsDailyMapper.java
@@ -0,0 +1,16 @@
+package com.liyang.staservice.mapper;
+
+import com.liyang.staservice.entity.StatisticsDaily;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 网站统计日数据 Mapper 接口
+ * </p>
+ *
+ * @author liyang
+ * @since 2021-05-20
+ */
+public interface StatisticsDailyMapper extends BaseMapper<StatisticsDaily> {
+
+}
